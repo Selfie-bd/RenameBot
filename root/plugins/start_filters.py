6fe25d9f172c7049cdfa2cdd327b49f89e05053d
Selfie SD <statuseditoff@gmail.com>
2022-06-11 23:09:04 +0530

    Update start_filters.py

--- root/plugins/start_filters.py
+++ root/plugins/start_filters.py
@@ -1,10 +1,4 @@
-'''
-RenameBot
-Thanks to Spechide Unkle as always for the concept  ♥️
-This file is a part of mrvishal2k2 rename repo 
-Dont kang !!!
-© Mrvishal2k2
-'''
+
 import os
 import logging
 logging.basicConfig(level=logging.DEBUG,
@@ -21,6 +15,13 @@ logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
 @Client.on_message(filters.command("help"))
 async def help_user(c,m):
+    button = [[
+               InlineKeyboardButton("Gʀᴏᴜᴘ", url="https://t.me/Groupdc"),
+               InlineKeyboardButton("Cʜᴀɴɴᴇʟ", url="https://t.me/groupdcbots")
+             ],[
+               InlineKeyboardButton("Sᴏᴜʀᴄᴇ Cᴏᴅᴇ", url="https://github.com/selfie-bd/AIOM-TG-RENAME-BOT")
+             ]]
+    markup = InlineKeyboardMarkup(button)
     try:
        await m.reply_text(Translation.HELP_USER,quote=True)
     except Exception as e:
@@ -29,7 +30,10 @@ async def help_user(c,m):
 @Client.on_message(filters.command("start"))
 async def start_msg(c,m):
     button = [[
-               InlineKeyboardButton("Owner ", url=f"https://t.me/{Config.OWNER_USERNAME}")
+               InlineKeyboardButton("Gʀᴏᴜᴘ", url="https://t.me/GroupDc"),
+               InlineKeyboardButton("Cʜᴀɴɴᴇʟ", url="https://t.me/groupdcbots")
+             ],[
+               InlineKeyboardButton("Sᴏᴜʀᴄᴇ Cᴏᴅᴇ", url="https://github.com/selfie-bd/AIOM-TG-RENAME-BOT")
              ]]
     markup = InlineKeyboardMarkup(button) 
     try:
