657d1fa131197808b87bba4b788368c773f7f747
Selfie SD <statuseditoff@gmail.com>
2022-06-19 13:13:15 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -36,7 +36,7 @@ class Bot(Client):
 
     def __init__(self):
         super().__init__(
-            session_name="RENAMEBOT",
+            RENAMEBOT,
             api_id=Config.APP_ID,
             api_hash=Config.API_HASH,
             bot_token=Config.TG_BOT_TOKEN,
